64b76477ab739a2fc1b5d7e10049876d4aefd38a
suresh sunuguri <sunuguri.suresh@gmail.com>
2026-06-21 17:38:45 -0400

    updated with GraphQL Query
    updated with GraphQL Query Mappings
    Setting up play area to use in browser

--- Graphql/src/main/java/com/gql/controller/ProductController.java
+++ Graphql/src/main/java/com/gql/controller/ProductController.java
@@ -3,28 +3,33 @@ package com.gql.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import com.gql.entity.Product;
 import com.gql.service.ProductService;
 
-@RestController
-@RequestMapping("/products")
+// Changing from @RestController to @Controller to avoid conflict with GraphQL endpoint
+//@RestController  
+//@RequestMapping("/products")
+@Controller
 public class ProductController {
 
 	@Autowired
 	private ProductService productService;
 	
-	@GetMapping
+	@QueryMapping
 	public List<Product> getAllProducts() {
 		return productService.getAllProducts();
 	}
 	
-	@GetMapping("/{category}")
-	public List<Product> getProductByCategory(@PathVariable String category) {
+	@QueryMapping
+	public List<Product> getProductByCategory(@Argument String category) {
 		return  productService.getProductByCategory(category);
 				//).elseThrow(() -> new RuntimeException("No products found in category: " + category));
 	}
