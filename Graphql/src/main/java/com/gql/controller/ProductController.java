6bf60ddacc3f5c8a96990b44ec78fc5232b56bf0
suresh sunuguri <sunuguri.suresh@gmail.com>
2026-06-21 18:21:52 -0400

    Updated with Mutation
    Updated the queries with Mutation (Update and Save methods)

--- Graphql/src/main/java/com/gql/controller/ProductController.java
+++ Graphql/src/main/java/com/gql/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,5 +33,22 @@ public class ProductController {
 	public List<Product> getProductByCategory(@Argument String category) {
 		return  productService.getProductByCategory(category);
 				//).elseThrow(() -> new RuntimeException("No products found in category: " + category));
+	}
+	
+	//Save using GraphQL Mutation
+	@MutationMapping
+	public Product updateStock(@Argument Integer id, @Argument Integer stock) {
+		return productService.updateStock(id, stock);
+	}
+	
+	@MutationMapping
+	public Product receivedNewPrice(@Argument Integer id, @Argument double price) {
+		return productService.receivedNewPrice(id, price);
+	}
+	
+	@MutationMapping
+	public Product saveProduct(@Argument String name, @Argument String category, @Argument Integer stock, @Argument double price) {
+		Product newProduct = new Product(name, category, stock, price);
+		return productService.getProductRepository().save(newProduct);
 	}
 }
